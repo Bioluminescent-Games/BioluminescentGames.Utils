f5f9478065fc89d0d387266a80149ecfa1b2a5ec
BeefyAndTheDucks <72296886+BeefyAndTheDucks@users.noreply.github.com>
2026-02-04 18:39:52 +0100

    Fix InstallDirectory

--- Runtime/Components/OpenExplorerComponent.cs
+++ Runtime/Components/OpenExplorerComponent.cs
@@ -12,31 +12,37 @@ namespace BioluminescentGames.Utils.Components
 {
     public class OpenExplorerComponent : MonoBehaviour
     {
-        [HelpBox("Supplies 2 UnityEvent compatible functions. You can also use ScriptableEvents")]
-        [SerializeField] private ScriptableEvent scriptableEvent;
-        [SerializeField] private ScriptableEvent<string> scriptableEventWithPath;
-
+        private enum Path
+        {
+            InstallLocation,
+            DataPath,
+            PersistentDataPath,
+            StreamingAssets,
+            CustomWithPlaceholders,
+        }
         private enum Type
         {
             File,
             Folder
         }
 
+        [HelpBox("Supplies 2 UnityEvent compatible functions. You can also use ScriptableEvents")]
+        [SerializeField] private EditorAttributes.Void helpBoxVoid;
+        [SerializeField] private ScriptableEvent scriptableEvent;
+        [SerializeField] private ScriptableEvent<string> scriptableEventWithPath;
+
+        [Space(2)]
         [SerializeField] private Type type;
-
-        private enum Path
-        {
-            InstallLocation,
-            PersistentDataPath,
-            StreamingAssets,
-            CustomWithPlaceholders,
-        }
-
         [SerializeField] private Path pathPreset;
 
+        [Space(2)]
         [ShowField(nameof(pathPreset), Path.CustomWithPlaceholders)]
-        [HelpBox("Valid Placeholders:\n[InstallLocation] = Install Location\n[PDP] = PersistentDataPath\n[SA] = StreamingAssets")]
+        [HelpBox("Valid Placeholders:\n[InstallLocation] = Install Location\n[DP] = DataPath\n[PDP] = PersistentDataPath\n[SA] = StreamingAssets")]
+        [SerializeField] private EditorAttributes.Void placeholdersVoid;
+        [ShowField(nameof(pathPreset), Path.CustomWithPlaceholders)]
         [SerializeField] private string pathToOpen = "";
+
+        private string InstallDirectory => Directory.GetParent(Application.dataPath)!.FullName;
 
         private void Awake()
         {
@@ -52,6 +58,9 @@ namespace BioluminescentGames.Utils.Components
             switch (pathPreset)
             {
                 case Path.InstallLocation:
+                    OpenExplorerToPath(InstallDirectory);
+                    break;
+                case Path.DataPath:
                     OpenExplorerToPath(Application.dataPath);
                     break;
                 case Path.PersistentDataPath:
@@ -61,7 +70,7 @@ namespace BioluminescentGames.Utils.Components
                     OpenExplorerToPath(Application.streamingAssetsPath);
                     break;
                 case Path.CustomWithPlaceholders:
-                    string path = pathToOpen.Replace("[InstallLocation]", Application.dataPath).Replace("[PDP]", Application.persistentDataPath)
+                    string path = pathToOpen.Replace("[InstallLocation]", InstallDirectory).Replace("[DP]", Application.dataPath).Replace("[PDP]", Application.persistentDataPath)
                         .Replace("[SA]", Application.streamingAssetsPath);
                     OpenExplorerToPath(path);
                     break;
