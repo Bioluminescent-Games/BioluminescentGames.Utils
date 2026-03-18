165fddd3de140a314611f628c099c7d2ec45e3e9
BeefyAndTheDucks <72296886+BeefyAndTheDucks@users.noreply.github.com>
2026-03-18 21:17:52 +0100

    Whoops

--- Runtime/Systems/Settings/UI/Metadata/OptionUIMetadata.cs
+++ Runtime/Systems/Settings/UI/Metadata/OptionUIMetadata.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 namespace BioluminescentGames.Utils.Systems.Settings.UI.Metadata
 {
-    public abstract class OptionUIMetadata : MonoBehaviour
+    public class OptionUIMetadata : MonoBehaviour
     {
         [field: SerializeField] public TMP_Text Title { get; private set; }
 
         public event Action OnDirty;
-        
+
         protected void SetDirty() => OnDirty?.Invoke();
 
         private void OnDestroy()
