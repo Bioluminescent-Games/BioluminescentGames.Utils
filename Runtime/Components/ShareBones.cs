12cd66a6f5f23ab5618a4f20b4f1fb5420dbf069
Simon Lotzfeldt <72296886+BeefyAndTheDucks@users.noreply.github.com>
2026-05-31 22:55:45 +0200

    Make ShareBones more aggressive

--- Runtime/Components/ShareBones.cs
+++ Runtime/Components/ShareBones.cs
@@ -1,17 +1,43 @@
+#if EDITOR_ATTRIBUTES
+using EditorAttributes;
+#endif
+using BioluminescentGames.Utils.MonoBehaviourExtensions;
 using UnityEngine;
 
 namespace BioluminescentGames.Utils.Runtime
 {
-    [RequireComponent(typeof(SkinnedMeshRenderer))]
     [DisallowMultipleComponent]
-    public class ShareBones : MonoBehaviour
+    public class ShareBones : BioluminescentBehaviour
     {
+        #if EDITOR_ATTRIBUTES
+        [Required]
+        #endif
         [SerializeField] private SkinnedMeshRenderer master;
+        
+        #if EDITOR_ATTRIBUTES
+        [HelpBox("If Slave is null, it will automatically try to grab one from this GameObject")]
+        #endif
+        [Tooltip("If this is null, it will automatically try to grab one from this GameObject")]
+        [SerializeField] private SkinnedMeshRenderer slave;
 
-        private void Start()
+        private int _recheckCounter;
+        
+        private void Awake()
         {
-            SkinnedMeshRenderer slave = GetComponent<SkinnedMeshRenderer>();
+            if (!slave)
+                slave = GetComponent<SkinnedMeshRenderer>();
+        }
 
+        public override void OnUpdate()
+        {
+            if (!slave || !master)
+                return;
+
+            _recheckCounter--;
+            if (_recheckCounter > 0)
+                return;
+            _recheckCounter = 60;
+            
             slave.bones = master.bones;
             slave.rootBone = master.rootBone;
         }
