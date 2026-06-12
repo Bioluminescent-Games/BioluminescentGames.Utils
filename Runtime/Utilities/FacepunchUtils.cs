4dbd09a2751b81ceff311d4c821c563e6024d08d
Simon Lotzfeldt <72296886+BeefyAndTheDucks@users.noreply.github.com>
2026-06-12 12:58:44 +0200

    Improve FacepunchUtils

--- Runtime/Utilities/FacepunchUtils.cs
+++ Runtime/Utilities/FacepunchUtils.cs
@@ -17,6 +17,7 @@ namespace BioluminescentGames.Utils.Runtime
     public static class FacepunchUtils
     {
         private static readonly Dictionary<ulong, Texture2D> AvatarCache = new();
+        private static readonly List<ulong> AvatarsUnderway = new();
         
         public static Texture2D Convert(this Image image)
         {
@@ -32,9 +33,9 @@ namespace BioluminescentGames.Utils.Runtime
             {
                 for (int y = 0; y < image.Height; y++)
                 {
-                    Color p = image.GetPixel(x, y);
+                    Color pixelCol = image.GetPixel(x, y);
                     avatar.SetPixel(x, (int)image.Height - y,
-                        new UnityEngine.Color(p.r / 255.0f, p.g / 255.0f, p.b / 255.0f, p.a / 255.0f));
+                        new UnityEngine.Color(pixelCol.r / 255.0f, pixelCol.g / 255.0f, pixelCol.b / 255.0f, pixelCol.a / 255.0f));
                 }
             }
 	
@@ -42,7 +43,11 @@ namespace BioluminescentGames.Utils.Runtime
             return avatar;
         }
 
+        #if UNITASK
+        private static async UniTask<Image?> GetAvatarInternal(SteamId steamId)
+        #else
         private static async Task<Image?> GetAvatarInternal(SteamId steamId)
+        #endif
         {
             try
             {
@@ -61,14 +66,19 @@ namespace BioluminescentGames.Utils.Runtime
         public static async Task<Texture2D> GetAvatar(SteamId steamId)
 #endif
         {
+            await UniTask.WaitWhile(() => AvatarsUnderway.Contains(steamId.Value));
+            
             if (AvatarCache.TryGetValue(steamId, out Texture2D cachedAvatar))
                 return cachedAvatar;
+            
+            AvatarsUnderway.Add(steamId.Value);
             
             Image? rawAvatar = await GetAvatarInternal(steamId);
             if (rawAvatar == null)
                 return null;
             Texture2D avatar = rawAvatar.Value.Convert();
             AvatarCache.Add(steamId, avatar);
+            AvatarsUnderway.Remove(steamId.Value);
             return avatar;
         }
 
